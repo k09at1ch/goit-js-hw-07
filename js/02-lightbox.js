57b017bee783128d9f1dd56177ce652c3e558163
k09at1ch <newwerasik228@gmail.com>
2023-04-09 13:32:38 +0300

    how 42 files???

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,18 +1,25 @@
-import { galleryItems } from './gallery-items.js'
+import { galleryItems } from './gallery-items.js';
+
+
 // Change code below this line
 const galleryMarkup = galleryItems
-.map(
-item =>
-`<li class="gallery__item"> <a class="gallery__link" href="${item.original}"> <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-caption="${item.description}" /> </a> </li>`
-)
-.join('');
+  .map(
+    item =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-caption="${item.description}" />
+        </a>
+      </li>`
+  )
+  .join('');
 
 const galleryContainer = document.querySelector('.gallery');
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
 const lightbox = new SimpleLightbox('.gallery a', {
-captions: true,
-captionDelay: 250,
-});
+    captions: true,
+    captionsDelay: 250,
+  });
+  
 
 console.log(galleryItems);
