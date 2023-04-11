e38cb0441e1adbbd1726c7e31c6f8a875458954f
k09at1ch <newwerasik228@gmail.com>
2023-04-11 23:22:22 +0300

    ав

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,7 +6,7 @@ const galleryMarkup = galleryItems
   .map(
     item =>
       `<li class="gallery__item">
-      <a class="gallery__link" href="#" data-src="${item.original}">
+      <a class="gallery__link" href="${item.original}">
           <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
         </a>
       </li>`
